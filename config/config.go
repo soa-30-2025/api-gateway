ab621d53dfe07ef38a34f77839a9817a773c614d
Stefan Nikolovski <nikolovski.stefan14@gmail.com>
2025-10-24 18:35:12 +0200

    feat: registered payment service

--- config/config.go
+++ config/config.go
@@ -11,9 +11,10 @@ type Config struct {
 	Address                   string
 	StakeHolderServiceAddress string
 	AuthServiceAddress        string
-  	BlogServiceAddress		  string
+	BlogServiceAddress        string
 	FollowerServiceAddress    string
-	TourServiceAddress		  string
+	TourServiceAddress        string
+	PaymentServiceAddress     string
 }
 
 func GetConfig() Config {
@@ -25,9 +26,10 @@ func GetConfig() Config {
 	return Config{
 		StakeHolderServiceAddress: os.Getenv("STAKEHOLDER_SERVICE_ADDRESS"),
 		AuthServiceAddress:        os.Getenv("AUTH_SERVICE_ADDRESS"),
-		BlogServiceAddress:		   os.Getenv("BLOG_SERVICE_ADDRESS"),
+		BlogServiceAddress:        os.Getenv("BLOG_SERVICE_ADDRESS"),
 		FollowerServiceAddress:    os.Getenv("FOLLOWER_SERVICE_ADDRESS"),
-		TourServiceAddress:    	   os.Getenv("TOUR_SERVICE_ADDRESS"),
+		TourServiceAddress:        os.Getenv("TOUR_SERVICE_ADDRESS"),
+		PaymentServiceAddress:     os.Getenv("PAYMENT_SERVICE_ADDRESS"),
 		Address:                   os.Getenv("GATEWAY_ADDRESS"),
 	}
 }
